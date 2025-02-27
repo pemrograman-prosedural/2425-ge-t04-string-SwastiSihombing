20cce5612960c50f8b106ef44a96170134174a20
SwastiSihombing <swastisihombing@gmail.com>
2025-02-27 14:58:31 +0700

    PAIR 12
    BERHASIL

--- t04_01.c
+++ t04_01.c
@@ -1,10 +1,26 @@
-// NIM - Name
-// NIM - Name
+// 12S24001 - Fredrick Laurensius Aritonang
+// 12S24030 - Swasti Maristella Sihombing
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main(int _argv, char **_argc) {
-  // codes
+  int main(int _argv, char **_argc) {
+
+  char msk[60];
+  int i;
+  int ASCII;
+  char ASCII2;
+
+  scanf("%60s", msk);
+  for (i=0; i< strlen(msk)- 2; i+= 3){
+    sscanf(&msk[i],"%03d", &ASCII);
+    ASCII2 = (char) ASCII;
+    printf("%c", ASCII2);
+  }
+
+  printf("\n");
 
   return 0;
+
 }
--- t04_02.c
+++ t04_02.c
@@ -1,10 +1,19 @@
-// NIM - Name
-// NIM - Name
+// 12S24001 - Fredrick Laurensius Aritonang
+// 12S24030 - Swasti Maristella Sihombing
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main(int _argv, char **_argc) {
-  // codes
+  char msk[60];
+  int i;
+
+  fgets(msk, 60, stdin);
+
+  for (i = 0; i < strlen(msk) - 1; i++){
+    printf("%03d", msk[i]);
+  }
 
   return 0;
 }
